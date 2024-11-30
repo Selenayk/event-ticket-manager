456543fba16e7d3e7a24ef8bb92a2c87d3478fd1
Selenay <selenaykeskinm@gmail.com>
2024-11-30 13:10:31 +0300

    Initial commit: Set up project structure and add dependencies
    - Created the basic project structure with folders: controller, dto, service, repository, etc.
    - Added dependencies for Spring Boot, Spring Web, and Spring Data JPA.
    - Initialized application.properties for database connection.

---
+++ event-ticket-manager-backend/src/main/java/com/selly/event_ticket_manager_backend/EventTicketManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.selly.event_ticket_manager_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventTicketManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventTicketManagerBackendApplication.class, args);
+	}
+
+}
---
+++ event-ticket-manager-backend/src/test/java/com/selly/event_ticket_manager_backend/EventTicketManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selly.event_ticket_manager_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventTicketManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
